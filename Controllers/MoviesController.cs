335bd24376c7b22e15e0cf25f2ac545edb0a1a42
ClarkeCDC <christian_clarke@outlook.com>
2021-06-15 22:55:57 +0100

    add

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,17 +20,38 @@ namespace MVCTut.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string SearchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
-            
-            if (!String.IsNullOrEmpty(SearchString))
+
+            if (!String.IsNullOrEmpty(searchString))
             {
-                movies = movies.Where(s => s.Title.Contains(SearchString));
+                movies = movies.Where(s => s.Title.Contains(searchString));
             }
-            
-            return View(await _context.Movie.ToListAsync());
+
+            if (!String.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
+        }
+
+        [HttpPost]
+        public string Index(string searchString, bool notUsed)
+        {
+            return "From [HttpPost]Index: filter on " + searchString;
         }
 
         // GET: Movies/Details/5
